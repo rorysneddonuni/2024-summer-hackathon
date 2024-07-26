1d3b767a37e079a07dc384b8b56237388ddfec4b
orlaishere <okeenan26@stals.org.uk>
2024-07-26 13:54:37 +0100

    Update script.js

--- primer/src/script.js
+++ primer/src/script.js
@@ -1,4 +1,4 @@
-const clientId = "your-client-id-here"; // Replace with your client id
+const clientId = "your-client-id-here"; // Replace with your client ID
 const code = undefined;
 
 if (!code) {
@@ -9,18 +9,18 @@ if (!code) {
     populateUI(profile);
 }
 
-async function redirectToAuthCodeFlow(clientId: string) {
+async function redirectToAuthCodeFlow(clientId) {
     // TODO: Redirect to Spotify authorization page
 }
 
-async function getAccessToken(clientId: string, code: string) {
+async function getAccessToken(clientId, code) {
   // TODO: Get access token for code
 }
 
-async function fetchProfile(token: string): Promise<any> {
+async function fetchProfile(token) {
     // TODO: Call Web API
 }
 
-function populateUI(profile: any) {
+function populateUI(profile) {
     // TODO: Update UI with profile data
 }
